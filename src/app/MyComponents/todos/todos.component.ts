88079b8441b933c5dd6f86fbd316718649f0edac
AsCodes21 <AsCodes21@gmail.com>
2022-02-05 20:09:24 +0530

    Models making and adding to screen

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo";
 
 @Component({
   selector: 'app-todos',
@@ -6,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  
+  todos:Todo[];
+  constructor() { 
+    this.todos = [
+      {
+        sno:1,
+        title:"This is title-1",
+        desc:"My description",
+        active:true        
+      },
+      {
+        sno:2,
+        title:"This is title-2",
+        desc:"My description",
+        active:true        
+      },
+      {
+        sno:3,
+        title:"This is title-3",
+        desc:"My description",
+        active:true        
+      }
+    ]
+   }
 
   ngOnInit(): void {
   }
